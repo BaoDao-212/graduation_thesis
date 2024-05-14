845dd62af18fab69ad70fa5cc951c45be267d3cb
BaoDao-212 <baodaoc1@gmail.com>
2024-05-15 00:21:10 +0700

    add module openai

--- back-end/src/modules/openai/openai.dto.ts
+++ back-end/src/modules/openai/openai.dto.ts
@@ -1,15 +1,20 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Answer } from 'src/entities/answer.entity';
+import { Post, PostStatus } from 'src/entities/post.entity';
+import { User } from 'src/entities/user.entity';
 import { CoreOutput } from 'src/modules/common/output.dto';
 
 export class OpenAiKeyInput {
-  @ApiProperty({ description: 'openAiKey' })
-  openAiKey: string;
+  @ApiProperty({ description: 'apikey' })
+  openAiKey:string;
 }
-export class OpenAiKeyOutput extends CoreOutput { 
-  @ApiProperty({ description: 'openAiKey' })
-  assistant?: string;
+export class OpenAiKeyOutput extends CoreOutput { }
+
+export class AnswerOutPut extends CoreOutput{
+  @ApiProperty({ description: 'answer' })
+  answer?: Answer;
 }
-export class GenerateQuestionsInput {
-  @ApiProperty({ description: 'file' })
- file: Express.Multer.File;
+export class AnswerListOutput extends CoreOutput {
+  @ApiProperty({ description: 'answers' })
+  answers?: Answer[];
 }
